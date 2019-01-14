fe3a17ef9e1a4700792eac027a8bee29d9c3ef90
Andy Muir <andy.muir@aimyourtechnology.com>
2019-01-14 17:36:53 +0000

    working

--- src/main/scala/com/github/muirandy/katas/scala/fibonacci/Fib.scala
+++ src/main/scala/com/github/muirandy/katas/scala/fibonacci/Fib.scala
@@ -1,8 +1,19 @@
 package com.github.muirandy.katas.scala.fibonacci
 
 class Fib() {
-  def calculateNthSequence(i: Int): Int = {
-    return 0
+  def calculateNthSequence(i: Int): Int = i match {
+    case 0 => return 0
+    case 1 => return 1
   }
 
+//    return 0
+
+//  def ordinal(number:Int) = number match {
+//    case 1 => println("1st")
+//    case 2 => println("2nd")
+//    //…
+//    case 9 => println("9th")
+//    case 10 => println("10th")
+//    case _ => println("Cannot do beyond 10")
+//  }
 }
