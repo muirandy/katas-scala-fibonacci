127a4ac18b9bdb2ec944efa9993562b6496c8f5d
Andy Muir <andy.muir@aimyourtechnology.com>
2019-01-14 09:21:28 +0000

    First test passing

---
+++ src/main/scala/com/github/muirandy/katas/scala/fibonacci/Fib.scala
@@ -0,0 +1,8 @@
+package com.github.muirandy.katas.scala.fibonacci
+
+class Fib() {
+  def calculateNthSequence(i: Int): Int = {
+    return 0
+  }
+
+}
---
+++ src/test/scala/com/github/muirandy/katas/scala/fibonacci/FibSpec.scala
@@ -0,0 +1,21 @@
+package com.github.muirandy.katas.scala.fibonacci
+
+import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.prop.TableDrivenPropertyChecks._
+
+class FibSpec extends FlatSpec with Matchers {
+
+  val pairs =
+    Table(
+      ("n", "f"),
+      (0, 0)
+    )
+
+  it should "work" in {
+    val fib = new Fib()
+
+    forAll(pairs) { (n: Int, f: Int) =>
+      fib.calculateNthSequence(n) should equal(f)
+    }
+  }
+}
