0beefd849b77bdfb8c546d8565518f8ec526fea2
Andy Muir <andy.muir@aimyourtechnology.com>
2019-01-14 17:41:25 +0000

    working

--- src/test/scala/com/github/muirandy/katas/scala/fibonacci/FibSpec.scala
+++ src/test/scala/com/github/muirandy/katas/scala/fibonacci/FibSpec.scala
@@ -11,7 +11,10 @@ class FibSpec extends FlatSpec with Matchers {
       (0, 0),
       (1, 1),
       (2, 1),
-      (3, 2)
+      (3, 2),
+      (4, 3),
+      (5, 5),
+      (6, 8)
     )
 
   it should "work" in {
