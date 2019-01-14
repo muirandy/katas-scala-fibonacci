b2e26956d3a6ab0852eea470e20869542bdc9921
Andy Muir <andy.muir@aimyourtechnology.com>
2019-01-14 17:57:20 +0000

    working

--- src/main/scala/com/github/muirandy/katas/scala/fibonacci/SimpleButInefficient.scala
+++ src/main/scala/com/github/muirandy/katas/scala/fibonacci/SimpleButInefficient.scala
@@ -3,7 +3,7 @@ package com.github.muirandy.katas.scala.fibonacci
 class SimpleButInefficient() extends FibonacciImplementation {
   def calculateNthSequence(i: Int): Int = i match {
     case 0 => return 0
-    case 1 => return 1
+    case 1 => return 0
     case _ => return calculateNthSequence(i-1) + calculateNthSequence(i-2)
   }
 }
