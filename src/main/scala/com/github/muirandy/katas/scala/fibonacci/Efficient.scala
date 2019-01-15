5a15f93e670bd55584bbc64c5ab5ad64a5ba0da3
Andy Muir <andy.muir@aimyourtechnology.com>
2019-01-15 09:45:09 +0000

    working

--- src/main/scala/com/github/muirandy/katas/scala/fibonacci/Efficient.scala
+++ src/main/scala/com/github/muirandy/katas/scala/fibonacci/Efficient.scala
@@ -2,16 +2,16 @@ package com.github.muirandy.katas.scala.fibonacci
 
 class Efficient {
 
-  def calculateNthSequence(i: Int): Long = i match {
-    case 0 => return 0
-    case 1 => return 1
+  def calculateNthSequence(i: Int): BigInt = i match {
+    case 0 => return BigInt(0)
+    case 1 => return BigInt(1)
     case _ => return calc(i)
   }
 
 
-  def calc(i: Int): Long = {
-    var allPairs = List[(Long,Long)]()
-    allPairs = (0L, 1L) :: (0L, 0L) :: allPairs
+  def calc(i: Int): BigInt = {
+    var allPairs = List[(BigInt,BigInt)]()
+    allPairs = (BigInt(0), BigInt(1)) :: (BigInt(0), BigInt(0)) :: allPairs
 
     for (index <- 2 to i) {
       allPairs = calculateNextPair(allPairs) :: allPairs
@@ -20,7 +20,7 @@ class Efficient {
     return allPairs.head._2
   }
 
-  def calculateNextPair(allPairs: List[(Long, Long)]): (Long, Long) = {
+  def calculateNextPair(allPairs: List[(BigInt, BigInt)]): (BigInt, BigInt) = {
     val left = allPairs.head._1 + 1
     val right = allPairs(0)._2 + allPairs(1)._2
     return (left, right)
--- src/test/scala/com/github/muirandy/katas/scala/fibonacci/FibSpec.scala
+++ src/test/scala/com/github/muirandy/katas/scala/fibonacci/FibSpec.scala
@@ -8,35 +8,35 @@ class FibSpec extends FlatSpec with Matchers {
   val easyPairs =
     Table(
       ("n", "f"),
-      (0, 0L),
-      (1, 1L),
-      (2, 1L),
-      (3, 2L),
-      (4, 3L),
-      (5, 5L),
-      (6, 8L),
-      (20, 6765L),
-      (25, 75025L)
+      (0, BigInt(0)),
+      (1, BigInt(1)),
+      (2, BigInt(1)),
+      (3, BigInt(2)),
+      (4, BigInt(3)),
+      (5, BigInt(5)),
+      (6, BigInt(8)),
+      (20, BigInt(6765)),
+      (25, BigInt(75025))
     )
 
   val bigPairs =
     Table(
       ("n", "f"),
-      (50, 12586269025L)
+      (50, BigInt("12586269025"))
     )
 
   it should "inefficient implementation should work" in {
-    forAll(easyPairs) { (n: Int, f: Long) =>
+    forAll(easyPairs) { (n: Int, f: BigInt) =>
       new SimpleButInefficient().calculateNthSequence(n) should equal(f)
     }
   }
 
   it should "work for efficient implementations" in {
     val implementation = new Efficient()
-      forAll(easyPairs) { (n: Int, f: Long) =>
+      forAll(easyPairs) { (n: Int, f: BigInt) =>
         implementation.calculateNthSequence(n) should equal(f)
       }
-      forAll(bigPairs) { (n: Int, f: Long) =>
+      forAll(bigPairs) { (n: Int, f: BigInt) =>
         implementation.calculateNthSequence(n) should equal(f)
       }
   }
