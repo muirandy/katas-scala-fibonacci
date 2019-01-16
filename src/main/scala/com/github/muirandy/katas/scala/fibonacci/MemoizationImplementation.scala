e9d66eb930f836ebe80f8c21918640aa03f0bf33
Andy Muir <andy.muir@aimyourtechnology.com>
2019-01-16 07:59:07 +0000

    working

--- src/main/scala/com/github/muirandy/katas/scala/fibonacci/MemoizationImplementation.scala
+++ src/main/scala/com/github/muirandy/katas/scala/fibonacci/MemoizationImplementation.scala
@@ -5,5 +5,10 @@ class MemoizationImplementation {
   def calculateNthSequence(i: Int): BigInt = i match {
     case 0 => return BigInt(0)
     case 1 => return BigInt(1)
+    case _ => return calc(i)
+  }
+
+  def calc(i: Int): BigInt = {
+    return BigInt(1)
   }
 }
